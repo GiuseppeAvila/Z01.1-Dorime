b2d38b40bdd702aa4e4af9c2f74e24c2036accf0
carolinechaim <caroline.chaim@gmail.com>
2020-03-05 15:09:43 -0300

    Todos do meu e um do rafa

--- Projetos/B-LogicaCombinacional/src/rtl/detectorDeMoedas.vhd
+++ Projetos/B-LogicaCombinacional/src/rtl/detectorDeMoedas.vhd
@@ -4,15 +4,17 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity detectorDeMoedas is
 	port (
     Q,D,N : in  STD_LOGIC;
-    cents     : out STD_LOGIC_VECTOR(4 downto 0);
+    cents     : out STD_LOGIC_VECTOR(4 downto 0));
 end entity;
 
 architecture arch of detectorDeMoedas is
 
 begin
 
-cents <= [11001] when Q;
-cents <= [1010] when D;
-cents <= [101] when N;
+	cents <= "00101" when (N = '1') else 
+	"01010" when (D = '1') else
+	"11001" when (Q = '1') else
+	"00000";
 
 end architecture;
+
