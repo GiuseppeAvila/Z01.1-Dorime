e8c8f7d1d29608df270f0ba56c85644b3f22610a
rafaelhbz <rafaelhbz@al.insper.edu.br>
2020-03-30 22:45:37 -0300

    Resolvendo problemas

--- Projetos/C-UnidadeLogicaAritmetica/src/rtl/HalfAdder.vhd
+++ Projetos/C-UnidadeLogicaAritmetica/src/rtl/HalfAdder.vhd
@@ -16,8 +16,7 @@ end entity;
 
 architecture rtl of HalfAdder is
   -- Aqui declaramos sinais (fios auxiliares)
-  -- e componentes (outros módulos) que serao
-  -- utilizados nesse modulo.
+  -- e componentes (outros módulos) que serao utilizadas nesse modulo
 
 begin
   -- Implementação vem aqui!
